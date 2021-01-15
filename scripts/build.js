2018110d1728b0610aae1d23445c535c8306b271
David Gauld <dcgauld@gmail.com>
2021-01-15 15:07:25 +0000

    build: add build script

---
+++ scripts/build.js
@@ -0,0 +1,35 @@
+const atImport = require('postcss-import');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const fs = require('fs-extra');
+const path = require('path');
+const postcss = require('postcss');
+
+const inputPath = path.resolve(__dirname, '../bloom.css');
+const inputFile = fs.readFileSync(inputPath, 'utf8');
+
+function process(minify = false) {
+  const plugins = [atImport(), autoprefixer()];
+  const outputPath = path.resolve(
+    __dirname,
+    `../build/bloom.${minify ? 'min.' : ''}css`
+  );
+
+  if (minify) {
+    plugins.push(cssnano({ preset: 'default' }));
+  }
+
+  return postcss(plugins)
+    .process(inputFile, { from: inputPath, to: outputPath })
+    .then((result) => {
+      fs.outputFileSync(outputPath, result.css);
+    });
+}
+
+function build() {
+  return Promise.all([process(), process(true)]);
+}
+
+build().then(() => {
+  console.log('Done.');
+});
